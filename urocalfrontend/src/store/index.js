58210aec0896d7b3e1bb45f6e9a66c258e48a167
henvisi-1994 <henvisi_1994@hotmail.com>
2021-02-13 19:13:38 -0500

    Revert "Recuperacion de Archivos"

--- urocalfrontend/src/store/index.js
+++ urocalfrontend/src/store/index.js
@@ -24,6 +24,7 @@ import ModuloMonitoreoRoedorCentroAcopio from './modules/ModuloMonitoreoRoedorCe
 import ModuloMonitoreoRoedorFinca from './modules/ModuloMonitoreoRoedorFinca';
 import ModuloLimpiezaVehiculo from './modules/ModuloLimpiezaVehiculo';
 import ModuloVehiculo from './modules/ModuloVehiculo';
+import ModuloReporteEmbarque from './modules/ModuloReporteEmbarque';
 import ModuloPoda from './modules/ModuloPoda';
 import ModuloFertilizante from './modules/ModuloFertilizante';
 import ModuloFitosanitario from './modules/ModuloFitosanitario';
@@ -108,6 +109,7 @@ export default new Vuex.Store({
     moduloMonitoreoRoedorFinca: ModuloMonitoreoRoedorFinca,                 // Instancia al ModuloMonitoreoRoedorFinca
     moduloLimpiezaVehiculo: ModuloLimpiezaVehiculo,                         // Instancia al ModuloLimpiezaVehiculo
     moduloVehiculo: ModuloVehiculo,                                         // Instancia al ModuloVehiculo
+    moduloReporteEmbarque: ModuloReporteEmbarque,
     moduloPoda: ModuloPoda,                                                 // Instancia al ModuloPoda
     moduloFertilizante: ModuloFertilizante,                                 // Instancia al ModuloFertilizante
     moduloFitosanitario: ModuloFitosanitario,                               // Instancia al ModuloFitosanitario
