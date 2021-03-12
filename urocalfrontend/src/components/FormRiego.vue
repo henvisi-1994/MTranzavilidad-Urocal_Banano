5e39044f59fcdfca907c9a9e2ec65236a3f3d6f5
Jason Torres <nevermindjdt@gmail.com>
2021-03-11 20:07:08 -0500

    Modulo Riego Validacion Completa

--- urocalfrontend/src/components/FormRiego.vue
+++ urocalfrontend/src/components/FormRiego.vue
@@ -13,6 +13,7 @@
             placeholder="Finca"
             class="style-chooser"
             label="finnombrefinca"
+            @input="obtenerTodosLoteCultivadoDeFinca" 
             :reduce="(listaFinca) => listaFinca.fincaid"
             :options="listaFincaStore"
             :rules="[reglas.campoVacio(fincaid)]"
@@ -56,7 +57,7 @@
             v-model="modeloRiegoStore.cultivoid"
             placeholder="Cultivo"
             class="style-chooser"
-            label="cultivonombre"
+            label="detalles"
             :reduce="(listaCultivo) => listaCultivo.cultivoid"
             :options="listaCultivo"
             :rules="[reglas.campoVacio(modeloRiegoStore.cultivoid)]"
@@ -152,8 +153,9 @@ import { mapState } from "vuex";
 import vSelect from "vue-select";
 import "vue-select/dist/vue-select.css";
 
-import ServicioLote from "../services/ServicioLote";
-
+import servicioLote from "../services/ServicioLote";
+import servicioFinca from "../services/ServicioFinca";
+import servicioCultivo from "../services/ServicioCultivo";
 export default {
   name: "FormRiego",
 
@@ -161,14 +163,18 @@ export default {
     vSelect,
   },
 
+  mounted() {
+    this.obtenerTodosListaCultivo();
+    this.obtenerTodosFincas();
+  },
+
   data() {
     return {
       listaLote: [],
+      listaFinca:[],
       fincaid: '',
       loteid: '',
-      listaCultivo: [
-        {cultivoid: 1,
-        cultivonombre: 'Cultivo provicional 1'},]
+      listaCultivo: [],
     }
   },
 
@@ -207,45 +213,28 @@ export default {
     ...mapState("validacionForm", ["reglas"]),
   },
 
-  watch: {
-    fincaid(newValue, oldValue){
-      if(!this.fincaid){
-        this.listaLote = [];
-        this.loteid = '';
-      } else {
-        this.obtenerLotes();
-      }
-    },
-
-    'modeloRiegoStore.fincaid'(){
-      if(this.modeloRiegoStore.fincaid) {
-        this.fincaid = this.modeloRiegoStore.fincaid;
-        this.loteid = this.modeloRiegoStore.lotecultivadoid;
-      }
-    }
-  },
+ 
 
   methods: {
-    async obtenerLotes(){
-      let newListaLotes = []
-      let respuesta = await ServicioLote.obtenerTodosLote();
-      let lotes = await respuesta.data;
-      lotes.forEach((f) => {
-        newListaLotes.push(f);
-      });
-      this.listaLote = newListaLotes;
+
+
+    async obtenerTodosListaCultivo() {
+      let resultado = await servicioCultivo.obtenerTodosCultivoDetalles();
+      this.listaCultivo = resultado.data; 
+
+    },
+      async obtenerTodosFincas() {
+      let resultado = await servicioFinca.obtenerTodosFincas();
+      this.listaFinca = resultado.data;
+       
+    },
+      async obtenerTodosLoteCultivadoDeFinca() {
+      let resultado = await servicioLote.obtenerTodosLoteCultivadoDeFinca(this.fincaid);
+      this.listaLote = resultado.data; 
+      
     },
 
-    // Asignar metodo cuando se termine el modulo de cultivo
-    async obtenerCultivos(){
-      let listaCultivos = [];
-      let respuesta = await ServicioCultivo.obtenerTodosCultivo();
-      let cultivos = await respuesta.data;
-      cultivos.forEach((f) => {
-        listaCultivos.push(f);
-      });
-      this.listaCultivoStore = listaCultivos;
-    },
+    
 
     limpiarIds(){
       this.fincaid = '';
