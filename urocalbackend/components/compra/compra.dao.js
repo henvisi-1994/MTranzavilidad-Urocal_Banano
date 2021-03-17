8e666736a7ee9461a2f3e675eae9a20b0d255072
henvisi-1994 <henvisi_1994@hotmail.com>
2021-03-17 17:23:52 -0500

    Modulo de Compra (Cambios en la DAO)

--- urocalbackend/components/compra/compra.dao.js
+++ urocalbackend/components/compra/compra.dao.js
@@ -4,16 +4,23 @@ const pool = require('../../services/postgresql/index');
 module.exports = {
     async getCompra() {
         let query = `SELECT c.compraid, c.comnumero, c.comsubtotal, c.comdescuentos, c.comotrosvalores, c.comtotal, 
-                        c.comobservaciones, TO_CHAR(c.comfechaemision, 'YYYY-MM-DD') as comfechaemision, c.productor, c.organizacion, c.cod, c.transporte, c.lugar 
-                     FROM compra c`;
+        c.comobservaciones, TO_CHAR(c.comfechaemision, 'YYYY-MM-DD') as comfechaemision, CONCAT(per.pernombres,' ',per.perapellidos) as productor, c.organizacion, c.cod,v.vehplaca as transporte, c.lugar 
+        FROM compra c
+        inner join guiaremision g on g.guiaremisionid = c.guiaremisionid
+        inner join productor p on p.productorid = g.productorid
+        inner join vehiculo v on v.vehiculoid= g.vehiculoid
+        inner join persona per on per.personaid = p.productorid`;
         let result = await pool.query(query);
         return result.rows; // Devuelve el array de json que contiene a todos los usuarios
     },
     async getCompras(id) {
-        let query = `select c.compraid, c.comnumero, c.comsubtotal, c.comdescuentos, c.comotrosvalores, c.comtotal, 
-                            c.comobservaciones, TO_CHAR(c.comfechaemision, 'YYYY-MM-DD') as comfechaemision, c.guiaremisionid,
-                            c.productor, c.organizacion, c.cod, c.transporte, c.lugar 
-                     FROM compra c WHERE compraid = '${id}'`;
+        let query = `SELECT c.compraid, c.comnumero, c.comsubtotal, c.comdescuentos, c.comotrosvalores, c.comtotal, 
+        c.comobservaciones, TO_CHAR(c.comfechaemision, 'YYYY-MM-DD') as comfechaemision, CONCAT(per.pernombres,' ',per.perapellidos) as productor, c.organizacion, c.cod,v.vehplaca as transporte, c.lugar 
+        FROM compra c
+        inner join guiaremision g on g.guiaremisionid = c.guiaremisionid
+        inner join productor p on p.productorid = g.productorid
+        inner join vehiculo v on v.vehiculoid= g.vehiculoid
+        inner join persona per on per.personaid = p.productorid WHERE c.compraid = '${id}'`;
         let result = await pool.query(query);
         return result.rows[0]; // Devuelve el json del usuario encontrado
     },
@@ -42,10 +49,10 @@ module.exports = {
         compra.comtotal = (parseInt(compra.comsubtotal) - parseInt(compra.comdescuentos) + parseInt(compra.comotrosvalores));*/
         // Registro en tabla compra
         query = `INSERT INTO public.compra (comnumero, comfechaemision, comsubtotal, 
-            comdescuentos, comotrosvalores, comtotal, comobservaciones, guiaremisionid) VALUES
+            comdescuentos, comotrosvalores, comtotal, comobservaciones, guiaremisionid, organizacion, cod, lugar) VALUES
                     (${compra.comnumero},'${compra.comfechaemision}',${compra.comsubtotal},
                     ${compra.comdescuentos},${compra.comotrosvalores},${compra.comtotal},'${compra.comobservaciones}',
-                    ${compra.guiaremisionid}) RETURNING compraid;`;
+                    ${compra.guiaremisionid}, ${compra.organizacion},${compra.cod},${compra.lugar}) RETURNING compraid;`;
         let result = await pool.query(query);
         //console.log(query)
         //console.log(result)
@@ -59,16 +66,11 @@ module.exports = {
         console.log(guia)
         compra.comtotal = (parseInt(compra.comsubtotal) - parseInt(compra.comdescuentos) + parseInt(compra.comotrosvalores));*/
         //modificando compra
-
-        let extra = ``;
-        if (compra.transporte) { extra = `transporte='${compra.transporte}',`;}
-
         query = `UPDATE public.compra SET comnumero = '${compra.comnumero}', comfechaemision = '${compra.comfechaemision}', 
         comsubtotal = ${compra.comsubtotal} , comdescuentos = ${compra.comdescuentos},
         comotrosvalores = ${compra.comotrosvalores},comtotal = ${compra.comtotal},
         comobservaciones = '${compra.comobservaciones}', guiaremisionid = ${compra.guiaremisionid} ,
-        productor = '${compra.productor}', organizacion='${compra.organizacion}', cod='${compra.cod}', 
-        ${extra} lugar='${compra.lugar}'
+        organizacion='${compra.organizacion}', cod='${compra.cod}',slugar='${compra.lugar}'
         where compraid = ${id}`;
         result = await pool.query(query);
         return result.rowCount; // Devuelve la cantidad de filas afectadas. Devuelve 1 si actualizó la compra y 0 sino lo hizo.
