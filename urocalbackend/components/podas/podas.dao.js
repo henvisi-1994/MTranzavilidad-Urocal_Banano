4290e0f64f2080fb185bfdbd7aa7b0d4983e8a5c
Darwin Calva <kalvadarwin1995@gmail.com>
2021-03-29 14:26:40 -0500

    Modulo poda completo

--- urocalbackend/components/podas/podas.dao.js
+++ urocalbackend/components/podas/podas.dao.js
@@ -3,34 +3,33 @@ const pool = require('../../services/postgresql/index');
 
 module.exports = {
     async getPodas() {
-        let query = `SELECT f.fincaid,f.fincodigo ,l.lotecultivadoid,c.cultivoid,
+
+        let query = `
+        SELECT f.fincaid,f.fincodigo ,l.lotecultivadoid,c.cultivoid,
         l.lotnumero,p.podaid,TO_CHAR(p.podfecha, 'YYYY-MM-DD') as podfecha,
-        p.podtipo,p.podhectareas,p.podcantidadplantas,p.podherramienta,p.podoperario 
-        FROM lotecultivado l INNER JOIN finca f ON f.fincaid= l.fincaid 
-        INNER JOIN cultivo c ON l.lotecultivadoid = c.lotecultivadoid 
-        INNER JOIN poda p ON c.cultivoid = p.cultivoid`;
+        p.podtipo,p.podhectareas,p.podcantidadplantas,p.podherramienta,p.podoperario,
+		concat(pr.pronombre, ' ', pr.provariedad) as cultivo
+		FROM poda p 
+		INNER JOIN cultivo c ON p.cultivoid=c.cultivoid
+		inner join producto pr on pr.productoid = c.productoid
+		inner join lotecultivado l on l.lotecultivadoid = c.lotecultivadoid
+		INNER JOIN finca f ON f.fincaid= l.fincaid
+        `;
         let result = await pool.query(query);
-
-        // console.log(result.rows); // Devuelve el array de json que contiene a todos los usuarios
-
-        // let query = `SELECT * FROM poda`;
-        // let result = await pool.query(query);
         return result.rows; // Devuelve el array de json que contiene a todos los usuarios
     },
 
     async getPoda(id) {
-        // let query = `SELECT f.fincaid,f.fincodigo ,l.lotecultivadoid,c.cultivoid,
-        // l.lotnumero,p.podaid,TO_CHAR(p.podfecha, 'YYYY-MM-DD') as podfecha,
-        // p.podtipo,p.podhectareas,p.podcantidadplantas,p.podherramienta,p.podoperario 
-        // FROM lotecultivado l INNER JOIN finca f ON f.fincaid= l.fincaid 
-        // INNER JOIN cultivo c ON l.lotecultivadoid = c.lotecultivadoid 
-        // INNER JOIN poda p ON c.cultivoid = p.cultivoid WHERE p.podaid = ${id}`;
-        
-        // let query = `SELECT * FROM poda WHERE podaid = ${id}`;
-                let query = `SELECT f.fincaid,f.fincodigo ,l.lotecultivadoid,c.cultivoid,l.lotnumero,p.podaid,p.podfecha,p.podtipo,p.podhectareas,p.podcantidadplantas,p.podherramienta,p.podoperario
-        FROM lotecultivado l INNER JOIN finca f ON f.fincaid= l.fincaid
-        INNER JOIN cultivo c ON l.lotecultivadoid = c.lotecultivadoid
-        INNER JOIN poda p ON c.cultivoid = p.cultivoid AND p.podaid = ${id}`;
+        let query = `
+        SELECT f.fincaid,f.fincodigo ,l.lotecultivadoid,c.cultivoid,
+        l.lotnumero,p.podaid,TO_CHAR(p.podfecha, 'YYYY-MM-DD') as podfecha,
+        p.podtipo,p.podhectareas,p.podcantidadplantas,p.podherramienta,p.podoperario,
+		concat(pr.pronombre, ' ', pr.provariedad) as cultivo
+		FROM poda p 
+		INNER JOIN cultivo c ON p.cultivoid=c.cultivoid
+		inner join producto pr on pr.productoid = c.productoid
+		inner join lotecultivado l on l.lotecultivadoid = c.lotecultivadoid
+		INNER JOIN finca f ON f.fincaid= l.fincaid WHERE p.podaid = ${id}`;
         let result = await pool.query(query);
         return result.rows[0]; // Devuelve el json del usuario encontrado
     },
