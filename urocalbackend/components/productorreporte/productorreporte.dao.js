d11287bfb362af3bb96ff028aa01699042f3f9e6
Samuel Calderón Aguirre <44515238+samcalderonaguirre@users.noreply.github.com>
2021-05-02 16:31:37 -0500

    Cambios en reporte de productores (Validaciones de Fecha)

--- urocalbackend/components/productorreporte/productorreporte.dao.js
+++ urocalbackend/components/productorreporte/productorreporte.dao.js
@@ -7,10 +7,21 @@ module.exports = {
     // SELECT: Devuelve todos los registros
     async obtenerProductor(id) {
         let query = `SELECT p.personaid, p.percedula, p.perapellidos, p.pernombres, p.perdireccion, p.pertelefono, p.perwhatsapp, p.peremail, p.pergenero, 
+                    extract(year from current_date) - extract(year from (SELECT TO_DATE(p.perfechanacimiento,'YYYY-MM-DD'))) AS edad, c.ciudadid, c.ciudadnombre, prod.productoridioma 
+                    FROM persona p, ciudad c, productor prod 
+                    WHERE p.personaid = ${id} AND  p.ciudadnacimientoid = c.ciudadid AND p.personaid = prod.productorid;`;
+        let result = await pool.query(query);
+        return result.rows;                     // Devuelve el array de json
+    },
+
+    /*
+    async obtenerProductor(id) {
+        let query = `SELECT p.personaid, p.percedula, p.perapellidos, p.pernombres, p.perdireccion, p.pertelefono, p.perwhatsapp, p.peremail, p.pergenero, 
                     extract(year from current_date) - extract(year from (SELECT TO_DATE(p.perfechanacimiento,'DD/MM/YYYY'))) AS edad, c.ciudadid, c.ciudadnombre, prod.productoridioma 
                     FROM persona p, ciudad c, productor prod 
                     WHERE p.personaid = ${id} AND  p.ciudadnacimientoid = c.ciudadid AND p.personaid = prod.productorid;`;
         let result = await pool.query(query);
         return result.rows;                     // Devuelve el array de json
     },
+    */
 }
